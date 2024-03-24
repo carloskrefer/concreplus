0f6a949f85c15c6580a6e1cbb5e4ca9fa80df4a5
Carlos Krefer <carloskrefer@hotmail.com>
2024-03-24 00:12:39 -0300

    Adiciona rota para o painel de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PainelPortalAcessoComponent } from './painel-portal-acesso/painel-portal-acesso.component';
+import { PainelLoginComponent } from './painel-login/painel-login.component';
 
 const routes: Routes = [
-  { path: '', component: PainelPortalAcessoComponent }
+  { path: '', component: PainelPortalAcessoComponent },
+  { path: 'login', component: PainelLoginComponent }
 ];
 
 @NgModule({
